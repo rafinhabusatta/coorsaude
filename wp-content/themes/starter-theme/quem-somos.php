64aae3eb38173f6d44349f18b91195eb1cba1daf
rafinhabusatta <lacerdabusatta@gmail.com>
2020-04-21 18:42:36 -0300

    CoorSaúde - Criação da página Quem Somos; Adição do plugin CTP UI; Adição do plugin ADVANCED CUSTOM FIELDS;

---
+++ wp-content/themes/starter-theme/quem-somos.php
@@ -0,0 +1,388 @@
+<!-- Template name: Quem-Somos -->
+<?php get_header(); ?>
+<div class="container-fluid">
+    <div class="row p-3 p-md-5">
+        <div class="col-12">
+            <h2 class="greenTitle text-center mb-4">Quem Somos</h2>
+        </div>
+        <div class="col-12 col-md-8 my-auto">
+            <div class="p-3 p-md-5 box-sombra">
+                <p class="text-justify">
+                    A CoorSaúde é constituída pelas seguintes representações: dois docentes de cada um
+                    dos cursos da Saúde da UFRGS, indicados pelas respectivas Comissões de
+                    Graduação, dois docentes pela Pró-Reitoria de Graduação; um discente e um
+                    servidor técnico-administrativos, no cargo de Técnico em Assuntos Educacionais
+                    das Unidades participantes da Coordenadoria.
+                </p>
+            </div>
+        </div>
+        <div class="col-4 d-none d-md-block">
+            <img src="<?php bloginfo("template_directory"); ?>/assets/team.svg" alt="Ícone do espírito de time" class="mt-5">
+        </div>
+    </div>
+    <div class="row p-3 p-md-5 equipe justify-content-around">
+        <div class="col-12">
+            <h2 class="greenTitle text-center mb-4">Equipe</h2>
+        </div>
+        <div class="col-12">
+            <h3 class="font-weight-bold text-uppercase equipe-nomes mb-3">Núcleo de Coordenação</h3>
+        </div>
+        <div class="col-12 col-md-5 col-lg-4 mb-4">
+            <div class="b-left box-equipe">
+                <h3 class="font-weight-bold">
+                    Profa. Ramona Fernanda Ceriotti Toassi (Odontologia)
+                </h3>
+                <p class="mb-3">
+                    Coordenadora
+                </p>
+                <p>
+                    Contato: <a class="font-weight-bold" href="mailto: ramona.fernanda@ufrgs.br">ramona.fernanda@ufrgs.br</a>
+                </p>
+            </div>
+        </div>
+        <div class="col-12 col-md-5 col-lg-4 mb-4">
+            <div class="b-left box-equipe">
+                <h3 class="font-weight-bold">
+                    Profa. Saionara Araújo Wagner (Medicina Veterinária)
+                </h3>
+                <p class="mb-3">
+                    Vice-coordenadora
+                </p>
+                <p>
+                    Contato: <a class="font-weight-bold" href="mailto: saionara.wagner@ufrgs.br">saionara.wagner@ufrgs.br</a>
+                </p>
+            </div>
+        </div>
+        <div class="col-12 col-md-5 col-lg-4 mb-4">
+            <div class="b-left box-equipe">
+                <h3 class="font-weight-bold">
+                    Prof. Luiz Fernando Calage Alvarenga  (Fisioterapia)
+                </h3>
+                <p class="mb-3">
+                    Terceiro docente da coordenação
+                </p>
+                <p>
+                    Contato: <a class="font-weight-bold" href="mailto: luiz.alvarenga@ufrgs.br">luiz.alvarenga@ufrgs.br</a>
+                </p>
+            </div>
+        </div>
+        <div class="col-12">
+            <h3 class="font-weight-bold text-uppercase equipe-nomes my-3">Secretaria da CoorSaúde</h3>
+        </div>
+        <div class="col-12 col-md-5 col-lg-4 mb-4">
+            <div class="b-left box-equipe">
+                <h3 class="font-weight-bold">
+                    Márcio Hoff
+                </h3>
+                <p class="mb-3">
+                    Técnico em Assuntos Educacionais
+                </p>
+                <p>
+                    Contato: <a class="font-weight-bold" href="mailto: coordsaude@ufrgs.br">coordsaude@ufrgs.br</a>
+                </p>
+            </div>
+        </div>
+        <div class="col-12 col-md-5 col-lg-4 mb-4">
+            <div class="b-left box-equipe">
+                <h3 class="font-weight-bold">
+                    Maria Luiza Vieira Borges
+                </h3>
+                <p class="mb-3">
+                    Bolsista
+                </p>
+                <p>
+                    Contato: <a class="font-weight-bold" href="mailto: marialuiza_vborges@hotmail.com">marialuiza_vborges@hotmail.com</a>
+                </p>
+            </div>
+        </div>
+        <div class="col-12 col-md-5 col-lg-4 mb-4">
+            <div class="b-left box-equipe">
+                <h3 class="font-weight-bold">
+                    Rafael Lacerda Busatta
+                </h3>
+                <p class="mb-3">
+                    Bolsista de Informática
+                </p>
+                <p>
+                    Contato: <a class="font-weight-bold" href="mailto: rafael.busatta@inf.ufrgs.br">rafael.busatta@inf.ufrgs.br</a>
+                </p>
+            </div>
+        </div>
+        <div class="col-12">
+            <h3 class="font-weight-bold text-uppercase equipe-nomes my-3">Membros da CoorSaúde Representantes das COMGRADS</h3>
+        </div>
+        <div class="col-12 col-md-5 col-lg-4 mb-4">
+            <h3 class="font-weight-bold">Bacharelado em Saúde Coletiva</h3>
+            <div class="b-left box-equipe">
+                <h3 class="font-weight-bold">
+                    Frederico Viana Machado
+                </h3>
+                <p class="mb-3">
+                    Titular
+                </p>
+                <h3 class="font-weight-bold">
+                    Dário Frederico Pasche
+                </h3>
+                <p>
+                    Suplente
+                </p>
+            </div>
+        </div>
+        <div class="col-12 col-md-5 col-lg-4 mb-4">
+            <h3 class="font-weight-bold">Biomedicina</h3>
+            <div class="b-left box-equipe">
+                <h3 class="font-weight-bold">
+                    João Henrique Corrêa Kanan
+                </h3>
+                <p class="mb-3">
+                    Titular
+                </p>
+                <h3 class="font-weight-bold">
+                    Alex Sander da Rosa Araújo
+                </h3>
+                <p>
+                    Suplente
+                </p>
+            </div>
+        </div>
+        <div class="col-12 col-md-5 col-lg-4 mb-4">
+            <h3 class="font-weight-bold">Ciências Biológicas</h3>
+            <div class="b-left box-equipe">
+                <h3 class="font-weight-bold">
+                    Katia Silva
+                </h3>
+                <p class="mb-3">
+                    Titular
+                </p>
+                <h3 class="font-weight-bold">
+                    Mara Rejane Ritter
+                </h3>
+                <p>
+                    Suplente
+                </p>
+            </div>
+        </div>
+        <div class="col-12 col-md-5 col-lg-4 mb-4">
+            <h3 class="font-weight-bold">Educação Física</h3>
+            <div class="b-left box-equipe">
+                <h3 class="font-weight-bold">
+                    Luiz Fernando Silva Bilibio
+                </h3>
+                <p class="mb-3">
+                    Titular
+                </p>
+                <h3 class="font-weight-bold">
+                    José Geraldo Soares Damico
+                </h3>
+                <p>
+                    Suplente
+                </p>
+            </div>
+        </div>
+        <div class="col-12 col-md-5 col-lg-4 mb-4">
+            <h3 class="font-weight-bold">Enfermagem</h3>
+            <div class="b-left box-equipe">
+                <h3 class="font-weight-bold">
+                    Carmem Lucia Motin DuroNome
+                </h3>
+                <p class="mb-3">
+                    Titular
+                </p>
+                <h3 class="font-weight-bold">
+                    Erica Rosalba Mallmann Duarte
+                </h3>
+                <p>
+                    Suplente
+                </p>
+            </div>
+        </div>
+        <div class="col-12 col-md-5 col-lg-4 mb-4">
+            <h3 class="font-weight-bold">Farmácia</h3>
+            <div class="b-left box-equipe">
+                <h3 class="font-weight-bold">
+                    Diogo Pilger
+                </h3>
+                <p class="mb-3">
+                    Titular
+                </p>
+                <h3 class="font-weight-bold">
+                    Aline Rigon Zimmer
+                </h3>
+                <p>
+                    Suplente
+                </p>
+            </div>
+        </div>
+        <div class="col-12 col-md-5 col-lg-4 mb-4">
+            <h3 class="font-weight-bold">Fisioterapia</h3>
+            <div class="b-left box-equipe">
+                <h3 class="font-weight-bold">
+                    Luiz Fernando Calage Alvarenga
+                </h3>
+                <p class="mb-3">
+                    Titular
+                </p>
+                <h3 class="font-weight-bold">
+                    Ângela Peña Ghisleni
+                </h3>
+                <p>
+                    Suplente
+                </p>
+            </div>
+        </div>
+        <div class="col-12 col-md-5 col-lg-4 mb-4">
+            <h3 class="font-weight-bold">Fonoaudiologia</h3>
+            <div class="b-left box-equipe">
+                <h3 class="font-weight-bold">
+                    Ana Paula Rigatti Scherer
+                </h3>
+                <p class="mb-3">
+                    Titular
+                </p>
+                <h3 class="font-weight-bold">
+                    Márcio Pezzini França
+                </h3>
+                <p>
+                    Suplente
+                </p>
+            </div>
+        </div>
+        <div class="col-12 col-md-5 col-lg-4 mb-4">
+            <h3 class="font-weight-bold">Medicina</h3>
+            <div class="b-left box-equipe">
+                <h3 class="font-weight-bold">
+                    Rodrigo Caprio Leite de Castro
+                </h3>
+                <p class="mb-3">
+                    Titular
+                </p>
+                <h3 class="font-weight-bold">
+                    Roberto Mário Silveira Issler
+                </h3>
+                <p>
+                    Suplente
+                </p>
+            </div>
+        </div>
+        <div class="col-12 col-md-5 col-lg-4 mb-4">
+            <h3 class="font-weight-bold">Medicina Veterinária</h3>
+            <div class="b-left box-equipe">
+                <h3 class="font-weight-bold">
+                    Saionara Araújo Wagner
+                </h3>
+                <p class="mb-3">
+                    Titular
+                </p>
+                <h3 class="font-weight-bold">
+                    Márcia Monks Jantzen
+                </h3>
+                <p>
+                    Suplente
+                </p>
+            </div>
+        </div>
+        <div class="col-12 col-md-5 col-lg-4 mb-4">
+            <h3 class="font-weight-bold">Nutrição</h3>
+            <div class="b-left box-equipe">
+                <h3 class="font-weight-bold">
+                     Eliziane Nicolodi Francescato Ruiz
+                </h3>
+                <p class="mb-3">
+                    Titular
+                </p>
+                <h3 class="font-weight-bold">
+                    Ana Beatriz Almeida de Oliveira
+                </h3>
+                <p>
+                    Suplente
+                </p>
+            </div>
+        </div>
+        <div class="col-12 col-md-5 col-lg-4 mb-4">
+            <h3 class="font-weight-bold">Odontologia</h3>
+            <div class="b-left box-equipe">
+                <h3 class="font-weight-bold">
+                    Ramona Fernanda Ceriotti Toassi
+                </h3>
+                <p class="mb-3">
+                    Titular
+                </p>
+                <h3 class="font-weight-bold">
+                    Luciane Maria Pilotto
+                </h3>
+                <p>
+                    Suplente
+                </p>
+            </div>
+        </div>
+        <div class="col-12 col-md-5 col-lg-4 mb-4">
+            <h3 class="font-weight-bold">Psicologia</h3>
+            <div class="b-left box-equipe">
+                <h3 class="font-weight-bold">
+                    Vera Lúcia Pasini
+                </h3>
+                <p class="mb-3">
+                    Titular
+                </p>
+                <h3 class="font-weight-bold">
+                    Jaqueline Tittoni
+                </h3>
+                <p>
+                    Suplente
+                </p>
+            </div>
+        </div>
+        <div class="col-12 col-md-5 col-lg-4 mb-4">
+            <h3 class="font-weight-bold">Serviço Social</h3>
+            <div class="b-left box-equipe">
+                <h3 class="font-weight-bold">
+                    Vanessa Maria Panozzo
+                </h3>
+                <p class="mb-3">
+                    Titular
+                </p>
+                <h3 class="font-weight-bold">
+                    Míriam Thais Guterres Dias
+                </h3>
+                <p>
+                    Suplente
+                </p>
+            </div>
+        </div>
+        <div class="col-12 col-md-5 col-lg-4 mb-4">
+            <h3 class="font-weight-bold">Bacharelado em Políticas Públicas</h3>
+            <div class="b-left box-equipe">
+                <h3 class="font-weight-bold">
+                    Marília Luz David
+                </h3>
+                <p class="mb-3">
+                    Titular
+                </p>
+                <h3 class="font-weight-bold">
+                    Andréa Fachel Leal
+                </h3>
+                <p>
+                    Suplente
+                </p>
+            </div>
+        </div>
+        <div class="col-12 col-md-5 col-lg-4 mb-4 mr-lg-auto">
+            <h3 class="font-weight-bold">Representantes da Prograd</h3>
+            <div class="b-left box-equipe">
+                <h3 class="font-weight-bold">
+                    Letícia Prezzi Fernandes
+                </h3>
+                <p class="mb-3">
+                    Titular
+                </p>
+                <h3 class="font-weight-bold">
+                    Cíntia Inês Boll
+                </h3>
+                <p>
+                    Suplente
+                </p>
+            </div>
+        </div>
+    </div> 
+</div>
+<?php get_footer(); ?>
