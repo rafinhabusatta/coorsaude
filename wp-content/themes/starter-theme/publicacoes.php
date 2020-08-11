29e2936b957ddf0c7ed79867d9ca8ccf3714e6d4
rafinhabusatta <lacerdabusatta@gmail.com>
2020-08-10 23:19:46 -0300

    Página-Publicação - Automatização;

--- wp-content/themes/starter-theme/publicacoes.php
+++ wp-content/themes/starter-theme/publicacoes.php
@@ -3,25 +3,27 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col-12 grid-container">
-                <div class="post">
-                    <h2 class="titulo">Título notícia</h2>
-                </div>
-                <div class="post">
-                    <h2 class="titulo">Título notícia</h2>
-                </div>
-                <div class="post">
-                    <h2 class="titulo">Título notícia</h2>
-                </div>
-                <div class="post">
-                    <h2 class="titulo">Título notícia</h2>
-                </div>
-                <div class="post">
-                    <h2 class="titulo">Título notícia</h2>
-                </div>
-                <div class="post">
-                    <h2 class="titulo">Título notícia</h2>
-                </div>
+                <?php 
+                    $args = array(
+                    'post_type' => 'post_publicacao',
+                    );
+                    $publicacao = new WP_Query ( $args );
+                ?>
+                <?php if ($publicacao -> have_posts()) : while ($publicacao -> have_posts()) : $publicacao -> the_post(); ?>
+                    
+                        <div class="post">
+                            <h2 class="titulo"><?php the_title() ?></h2>
+                        </div>
+                <?php endwhile; endif; ?>
             </div>
         </div>
     </div>
 <?php get_footer(); ?>
+
+<?php 
+    function create_publicacao($titulo){
+        $hmtl .= '<div class="post">';
+        $html .= '<h2 class="titulo"><?php the_title() ?></h2>';
+        $html .= '</div>';
+    }
+?>
