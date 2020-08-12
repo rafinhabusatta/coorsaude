14faf71800aa2be478cec22585a0ee6ab102289c
rafinhabusatta <lacerdabusatta@gmail.com>
2020-08-12 19:41:43 -0300

    Página-Publicações - adicao do single-publicacoes; Tentativa de criação de paginação, sem sucesso;

---
+++ wp-content/themes/starter-theme/single_publicacoes.php
@@ -0,0 +1,15 @@
+<?php get_header(); ?>
+
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-12">
+            <article>
+                <header>
+                    <h1>Título Notícia</h1>
+                </header>
+            </article>
+        </div>
+    </div>
+</div>
+
+<?php get_footer(); ?>
