1b05c20a4b580da20be6faab4b4922d5f616522d
rafinhabusatta <lacerdabusatta@gmail.com>
2020-08-12 22:42:24 -0300

    HEADER - Criação do header oficial, com a implementação do design criado também hoje; Adição da logo do TelessaúdeRS e UFRGS;

--- wp-content/themes/starter-theme/footer.php
+++ wp-content/themes/starter-theme/footer.php
@@ -1,12 +1,57 @@
 <footer class="footer mt-5">
-	<div class="container-fluid">
-		<div class="row p-3">
-			<!-- <div class="col-3 logo-footer">
-				<img src="<?php bloginfo('template_directory'); ?>/img/logo.png" alt="">
-			</div> -->
+	<div class="container-fluid grid-footer px-5">
+		<div class="row py-5 footer-1">
 			<div class="col-12 text-center">
-				Rua Ramiro Barcelos, 2777, Sala 163 - Porto Alegre/RS - (51) 3308-5512
+				Redes Sociais
 			</div>
+		</div>
+		<div class="row pt-3 footer-2">
+			<div class="col-12">	
+				<div class="footer-2-list">
+					<h2 class="footer-title">Cursos Presentes no Comitê Gestor</h2>
+					<ul class="list-1">
+						<li><a href="http://www.ufrgs.br/biomedicina/" target="_blank">Biomedicina</a></li>
+						<li><a href="https://www.ufrgs.br/biociencias/" target="_blank">Ciêcias Biológicas</a></li>
+						<li><a href="https://www.ufrgs.br/esefid/site/" target="_blank">Educação Física</a></li>
+						<li><a href="http://www.ufrgs.br/eenf/escola-de-enfermagem-da-ufrgs" target="_blank">Enfermagem</a></li>
+						<li><a href="https://www.ufrgs.br/farmacia/" target="_blank">Farmácia</a></li>
+						<li><a href="https://www.ufrgs.br/esefid/site/graduacao/fisioterapia" target="_blank">Fisioterapia</a></li>
+						<li><a href="http://www.ufrgs.br/odontologia/ensino/fono" target="_blank">Fonoaudiologia</a></li>
+					</ul>
+					<ul class="list-2">
+						<li><a href="https://www.ufrgs.br/famed/" target="_blank">Medicina</a></li>
+						<li><a href="https://www.ufrgs.br/favet/" target="_blank">Medicina Veterinária</a></li>
+						<li><a href="https://www.ufrgs.br/famed/?pag=arqs/graduacao-em-nutricao.php" target="_blank">Nutrição</a></li>
+						<li><a href="http://www.ufrgs.br/odontologia" target="_blank">Odontologia</a></li>
+						<li><a href="http://www.ufrgs.br/psicologia/" target="_blank">Psicologia</a></li>
+						<li><a href="http://www.ufrgs.br/saudecoletiva/" target="_blank">Saúde Coletiva</a></li>
+						<li><a href="http://www.ufrgs.br/psicologia/graduacao/servico-social" target="_blank">Serviço Social</a></li>
+					</ul>		
+				</div>	
+			</div>
+		</div>
+		<div class="row pt-3 footer-3">
+			<div class="col-12">		
+					<a href="<?php echo site_url(); ?>"><h2 class="footer-title">CoorSaúde</h2></a>
+					<ul>
+						<li><a href="<?php echo site_url(); ?>/quem-somos">Quem Somos</a></li>
+						<li><a href="<?php echo site_url(); ?>/estagios">Estágios</a></li>
+						<li><a href="<?php echo site_url(); ?>/cenarios-de-pratica">Cenários de Prática</a></li>
+						<li><a href="<?php echo site_url(); ?>/comite-gestor">Comitê Gestor</a></li>
+						<li><a href="<?php echo site_url(); ?>/disciplinas-integradoras">Disciplinas Integradoras</a></li>
+					</ul>		
+			</div>
+		</div>
+		<div class="row pt-3 footer-4">
+			<div class="col-12">			
+				<a href="<?php echo site_url(); ?>/contato"><h2 class="footer-title">Contato</h2></a>
+				<ul>
+					<li>Telefone:<a href="">(51) 3308.5512</a></li>
+					<li>Email: <a href="mailto:coordsaude@ufrgs.br">coordsaude@ufrgs.br</a></li>
+				</ul>	
+			</div>
+		</div>
+		<div class="row pt-5 pb-3 footer-5">
 			<div class="col-12 text-center font-weight-bold">
 				CoorSaúde - UFRGS
 			</div>
