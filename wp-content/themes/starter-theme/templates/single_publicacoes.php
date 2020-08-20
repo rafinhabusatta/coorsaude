397bd6812cb7dd8eeec516b6a2c1c1a95b912c3d
rafinhabusatta <lacerdabusatta@gmail.com>
2020-08-20 18:54:53 -0300

    Página-Publicacoes - Criação da pasta templates;

--- wp-content/themes/starter-theme/templates/single_publicacoes.php
+++ wp-content/themes/starter-theme/templates/single_publicacoes.php
@@ -1,10 +1,11 @@
+<!-- Template name: Single-Publicacao -->
 <?php get_header(); ?>
 
 <div class="container-fluid">
     <div class="row">
         <div class="col-12">
-            <article>
-                <header>
+            <article <?php post_class() ;?> id="post-<?php the_ID(); ?>">
+                <header class="row noticia">
                     <h1>Título Notícia</h1>
                 </header>
             </article>
