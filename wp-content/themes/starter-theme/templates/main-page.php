fa6033d28d2aa5c8303b867b37d5355639625e2a
rafinhabusatta <lacerdabusatta@gmail.com>
2020-10-04 19:03:08 -0300

    Home - Correção da margem na área Calendário; Adição de hover effect no link calendário; Remoção do carrossel; Finalização Definitiva da página home;

--- wp-content/themes/starter-theme/templates/main-page.php
+++ wp-content/themes/starter-theme/templates/main-page.php
@@ -8,75 +8,67 @@
                     <div class="carousel-item active bg-coor">
                         <h1 class="coor">Coor<span class="saude">Saúde</span></h1>
                     </div>
-                    <div class="carousel-item">
+                    <!-- <div class="carousel-item">
                         <img class="d-block w-100" src="<?php bloginfo("template_directory"); ?>/img/ufrgs2.jpg" alt="Segundo slide" width="800">
                     </div>
                     <div class="carousel-item">
                         <img class="d-block w-100" src="<?php bloginfo("template_directory"); ?>/img/ufrgs3.jpg" alt="Terceiro slide" width="800">
-                    </div>
+                    </div> -->
                 </div>
-                <a class="carousel-control-prev" href="#carousel-Coorsaude" role="button" data-slide="prev">
+                <!-- <a class="carousel-control-prev" href="#carousel-Coorsaude" role="button" data-slide="prev">
                     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                     <span class="sr-only">Anterior</span>
                 </a>
                 <a class="carousel-control-next" href="#carousel-Coorsaude" role="button" data-slide="next">
                     <span class="carousel-control-next-icon" aria-hidden="true"></span>
                     <span class="sr-only">Próximo</span>
-                </a>
+                </a> -->
             </div>
         </div>
     </div>
-    <!-- <div class="row">
-        <div class="col-12">
-            <img src="<?php bloginfo("template_directory"); ?>/img/ufrgs2.jpg" alt="" class="clip-svg">
-        </div>
-    </div>
-    <svg xmlns="http://www.w3.org/2000/svg" width="1020.287" height="584.242" viewBox="0 0 1020.287 584.242">
-        <defs>
-            <clipPath id="Caminho_6">
-                <path  data-name="Caminho 6" d="M-20998.639-20302c171.631-144.928,448.355,32.725,448.355,32.725s247.645,141.229,447.5-85.512,31.291-376.686,31.291-376.686l-928.047,6.291.9,423.182Z" transform="translate(20999 20943.596)" fill="#4fb793"/>
-            </clipPath>
-        </defs>    
-    </svg> -->
-    <div class="row px-3 pt-4 pb-5 px-md-5 m-lg-5 sobre">
+    <div class="row sobre">
         <div class="col-12 col-md-6 px-md-0">
             <h2 class="greenTitleSpecial text-center text-md-left mb-4">Sobre a CoorSaúde</h2>
             <p class="text-justify mb-md-0">
-                A Coordenadoria da Saúde (CoorSaúde) da UFRGS é uma instância vinculada à <a class="font-weight-bold" href="https://www.ufrgs.br/prograd/" target="_blank">Pró-Reitoria de Graduação(ProGrad)</a>,  cuja missão é integrar os cursos da saúde e articular as relações da Universidade com a rede do<span id="dots-sobre">...</span>
+                A Coordenadoria da Saúde (CoorSaúde) da UFRGS é uma instância vinculada à <a class="font-weight-bold" href="https://www.ufrgs.br/prograd/" target="_blank">Pró-Reitoria de Graduação(ProGrad)</a>,  cuja missão é integrar os cursos da saúde e articular as relações da Universidade com a rede do<span id="dots-sobre" class="d-xl-none">...</span><span id="more-sobre" class="d-none d-xl-inline">
+                    Sistema Único de Saúde (SUS). Suas ações buscam a facilitação do desenvolvimento de um Projeto Pedagógico Institucional que atenda às Diretrizes Curriculares Nacionais (DCN) dos cursos da saúde e a qualificação do SUS.
+                </span>
                 <span id="more-sobre" class="d-none">
-                    Sistema Único de Saúde (SUS).Suas ações buscam a facilitação do desenvolvimento de um Projeto Pedagógico Institucional que atenda às Diretrizes Curriculares Nacionais (DCN) dos cursos da saúde e a qualificação do SUS.
-                </span> 
-                <button class="font-weight-bold " id="btn-sobre" onclick="ReadSobre()">Leia mais...</button>
+                    Sistema Único de Saúde (SUS). Suas ações buscam a facilitação do desenvolvimento de um Projeto Pedagógico Institucional que atenda às Diretrizes Curriculares Nacionais (DCN) dos cursos da saúde e a qualificação do SUS.
+                </span>
+                <span class="font-weight-bold d-xl-none btn-leia" id="btn-sobre" onclick="ReadSobre()">Leia mais</span>
             </p>
         </div>
         <div class="col-12 col-md-6 text-right pr-md-0">
             <img src="<?php bloginfo("template_directory"); ?>/img/ufrgs2.jpg" alt="Imagem da fachada da Universidade Federal do Rio Grande do Sul">
         </div>
     </div>
-    <div class="row justify-content-between text-center acoes-desenvolvidas">
-        <div class="col-12 mb-3">
-            <h2 class="greenTitle mt-4">Ações Desenvolvidas</h2>
-        </div>
-        <div class="col-12 col-md- mb-5 box-sombra d-table caixa-acoes">
-            <p class="d-table-cell align-middle">Disciplinas Integradoras PIS I e PIS II</p>
-        </div>
-        <div class="col-12 col-md- mb-5 box-sombra d-table caixa-acoes">
-            <p class="d-table-cell align-middle">Comissão de Gestão e Acompanhamento Local (CGAL)</p>
-        </div>
-        <div class="col-12 col-md- mb-5 box-sombra d-table caixa-acoes">
-            <p class="d-table-cell align-middle">PET-Saúde na UFRGS</p>
+    <div class="acoes-bg px-3">
+        <div class="row justify-content-between text-center acoes-desenvolvidas">
+            <div class="col-12 mb-3">
+                <h2 class="greenTitle mt-4">Ações Desenvolvidas</h2>
+            </div>
+            <div class="col-12 col-md- col-lg-4 mb-5 box-sombra d-table caixa-acoes">
+                <p class="d-table-cell align-middle">Disciplinas Integradoras PIS I e PIS II</p>
+            </div>
+            <div class="col-12 col-md- col-lg-4 mb-5 box-sombra d-table caixa-acoes">
+                <p class="d-table-cell align-middle">Comissão de Gestão e Acompanhamento Local (CGAL)</p>
+            </div>
+            <div class="col-12 col-md- col-lg-4 mb-5 box-sombra d-table caixa-acoes">
+                <p class="d-table-cell align-middle">PET-Saúde na UFRGS</p>
+            </div>
         </div>
     </div>
-    <div class="row px-3 pt-5 pb-5 p-md-5 historico">
+    <div class="row px-3 pt-5 pb-5 p-md-5 my-lg-3 historico">
         <div class="col-12">
             <h2 class="greenTitle text-center mb-4">Histórico</h2>
         </div>
         <div class="col-12 px-md-0">
             <p class="text-justify mb-0">
-                A Coordenadoria da Saúde (CoorSaúde) da UFRGS é resultado do movimento de implantação das Diretrizes Curriculares Nacionais para a área da saúde (Edital SESu/MEC nº 4, de 10/12/1997 e Resoluções CNE/CES nos 3, 4 e 5/2001; 2, 3, 4, 5 e 6/2002; 1 e 2/2003 e 7 e 8/2004) e das Diretrizes Constitucionais Nacionais para o setor da saúde (Artigos 200, 205, 208 e 214,  da Constituição Federal de 1988), conforme desdobramentos da Lei de Diretrizes e Bases da Educação Nacional (Artigos 1º, 2º, 3º, 43 e 53) e da Lei Orgânica da Saúde (Artigos 13, 14, 15 e 27), além do consequente processo de mudança nos cursos da área.<span id="dots-historic">...</span>
+                A Coordenadoria da Saúde (CoorSaúde) da UFRGS é resultado do movimento de implantação das Diretrizes Curriculares Nacionais para a área da saúde (Edital SESu/MEC nº 4, de 10/12/1997 e Resoluções CNE/CES nos 3, 4 e 5/2001; 2, 3, 4, 5 e 6/2002; 1 e 2/2003 e 7 e 8/2004) e das Diretrizes Constitucionais Nacionais para o setor da saúde (Artigos 200, 205, 208 e 214,  da Constituição Federal de 1988), conforme desdobramentos da Lei de Diretrizes e Bases da Educação Nacional (Artigos 1º, 2º, 3º, 43 e 53) e da Lei Orgânica da Saúde (Artigos 13, 14, 15 e 27), além do consequente processo de mudança nos cursos da área. <span class="d-none d-xl-inline">Dentre as principais estratégias de atenção aos determinantes legais e orientações normativas está a necessidade de ações conjuntas e integradas entre os cursos. A partir de 2004 e em resposta à convocação nacional despertada pelos Ministérios da Educação e da Saúde designado por “AprenderSUS – o SUS e os cursos de graduação da área da saúde”, os cursos da área e afins passaram a discutir, por meio de grupos de estudos ou projetos correspondentes aos editais nacionais apresentados pela Política Nacional de Formação e Desenvolvimento para o Sistema Único de Saúde: Caminhos para a Educação Permanente em Saúde (Resolução CNS nº 335, 27/11/2003) a construção da integralidade da atenção à saúde, o trabalho em equipes multiprofissionais e interdisciplinares, a apropriação do sistema</span><span id="dots-historic">...</span>
 
                 <span id="more-historic" class="d-none">
-                    Dentre as principais estratégias de atenção aos determinantes legais e orientações normativas está a necessidade de ações conjuntas e integradas entre os cursos. A partir de 2004 e em resposta à convocação nacional despertada pelos Ministérios da Educação e da Saúde designado por “AprenderSUS – o SUS e os cursos de graduação da área da saúde”, os cursos da área e afins passaram a discutir, por meio de grupos de estudos ou projetos correspondentes aos editais nacionais apresentados pela Política Nacional de Formação e Desenvolvimento para o Sistema Único de Saúde: Caminhos para a Educação Permanente em Saúde (Resolução CNS nº 335, 27/11/2003) a construção da integralidade da atenção à saúde, o trabalho em equipes multiprofissionais e interdisciplinares, a apropriação do sistema de saúde vigente em nosso país e a educação permanente em docência e práticas pedagógicas na saúde.
+                    <span class="d-xl-none">Dentre as principais estratégias de atenção aos determinantes legais e orientações normativas está a necessidade de ações conjuntas e integradas entre os cursos. A partir de 2004 e em resposta à convocação nacional despertada pelos Ministérios da Educação e da Saúde designado por “AprenderSUS – o SUS e os cursos de graduação da área da saúde”, os cursos da área e afins passaram a discutir, por meio de grupos de estudos ou projetos correspondentes aos editais nacionais apresentados pela Política Nacional de Formação e Desenvolvimento para o Sistema Único de Saúde: Caminhos para a Educação Permanente em Saúde (Resolução CNS nº 335, 27/11/2003) a construção da integralidade da atenção à saúde, o trabalho em equipes multiprofissionais e interdisciplinares, a apropriação do sistema</span> de saúde vigente em nosso país e a educação permanente em docência e práticas pedagógicas na saúde.
 
                     Ao longo dos anos de 2006 e 2007 a UFRGS construiu, submeteu e aprovou junto ao Ministério da Saúde o Projeto de Extensão “Integralidade na saúde”. Este projeto foi intermediado pela Organização Pan-Americana da Saúde e desenvolvido com a participação de estudantes e professores dos cursos de graduação em Enfermagem, Educação Física, Farmácia, Medicina, Nutrição, Odontologia e Psicologia. Ao longo da execução do projeto, os professores envolvidos constataram a urgência de uma ação institucional que favorecesse e ampliasse a discussão e a ação acadêmica regular e oficial.
 
@@ -84,7 +76,7 @@
 
                     A Coordenadoria da Saúde da UFRGS surgiu, portanto, como uma proposta para contribuir com a definição de uma política inovadora, mas fundamentalmente contemporânea, de formação de profissionais da área da saúde ou com forte atuação no setor da saúde, no âmbito da Universidade e dos Serviços, tendo em vista implementar as Diretrizes Curriculares Nacionais em concordância com as políticas públicas dos setores da Educação Superior e do Sistema Único de Saúde, de modo especial a construção da integralidade e da interprofissionalidade nas atividades de ensino, pesquisa e extensão.
                 </span>
-                <button class="btn-leia" id="btn-historic" onclick="ReadHistoric()">Leia mais</button>
+                <span class="font-weight-bold btn-leia" id="btn-historic" onclick="ReadHistoric()">Leia mais</span>
             </p>
         </div>
     </div>
@@ -93,15 +85,15 @@
             <h2 class="text-uppercase font-weight-bold m-0 p-md-0">Seminário de 10 anos CoorSaúde</h2>
             <h3 class="text-uppercase p-md-0 mb-0">08 e 09 de novembro de 2018</h3>
             <p class="mt-4 mb-3 p-md-0 text-justify">
-                Em 2018, a CoorSaúde completou dez anos de atividade. Em comemoração realizou-se o seminário Balanço e Perspectivas de Reorientação Profissional e Integração Ensino-Serviço, com o debate de diversos temas<span id="dots-seminario">...</span>
+                Em 2018, a CoorSaúde completou dez anos de atividade. Em comemoração realizou-se o seminário <span class="font-italic">"Balanço e Perspectivas de Reorientação Profissional e Integração Ensino-Serviço"</span>, com o debate de diversos temas <span class="d-none d-xl-inline">“Balanço dos principais marcos da integração Universidade e SUS – COMO NOS ORGANIZAMOS JUNTOS?”, “Análise histórico-contextual e perspectivas da integração ensino-serviço-comunidade”, “Cenários e atores de transformação – disciplinas Práticas Integradas em Saúde I e II; PET-Saúde”, “Saúde e Movimentos Sociais - Roda de Conversa com lideranças dos movimentos sociais da saúde, Conselho Distrital de Saúde, Projeto Memórias de Barracão”, “Roda de conversa sobre Diretrizes Curriculares Nacionais (DCN)</span><span id="dots-seminario">...</span>
                 <span id="more-seminario" class="d-none">
-                    “Balanço dos principais marcos da integração Universidade e SUS – COMO NOS ORGANIZAMOS JUNTOS?”, “Análise histórico-contextual e perspectivas da integração ensino-serviço-comunidade”, “Cenários e atores de transformação – disciplinas Práticas Integradas em Saúde I e II; PET-Saúde”, “Saúde e Movimentos Sociais - Roda de Conversa com lideranças dos movimentos sociais da saúde, Conselho Distrital de Saúde, Projeto Memórias de Barracão”, “Roda de conversa sobre Diretrizes Curriculares Nacionais (DCN) nos cursos de saúde com Entidades Nacionais de Ensino, Conselhos Profissionais da Saúde e Coordenadores de Curso de Graduação”, “As DCN e a Reorientação da Formação Profissional dos Cursos da Saúde”, “Sistematização da trajetória de Reorientação dos Cursos de Graduação da Saúde na UFRGS”, “Apresentação da história de Reorientação de Formação dos Cursos da Área da Saúde (videos)” e “Análise da conjuntura da saúde e cenário de resistência e perspectivas da integração Universidade e SUS”. Também houve exposições de pôsteres e tendas dos movimentos sociais. O seminário reuniu 230 participantes, contando com a presença de estudantes, professores, trabalhadores e gestores do SUS, e representantes da gestão da Universidade.
+                    <span clas="d-xl-none">“Balanço dos principais marcos da integração Universidade e SUS – COMO NOS ORGANIZAMOS JUNTOS?”, “Análise histórico-contextual e perspectivas da integração ensino-serviço-comunidade”, “Cenários e atores de transformação – disciplinas Práticas Integradas em Saúde I e II; PET-Saúde”, “Saúde e Movimentos Sociais - Roda de Conversa com lideranças dos movimentos sociais da saúde, Conselho Distrital de Saúde, Projeto Memórias de Barracão”, “Roda de conversa sobre Diretrizes Curriculares Nacionais (DCN)</span> nos cursos de saúde com Entidades Nacionais de Ensino, Conselhos Profissionais da Saúde e Coordenadores de Curso de Graduação”, “As DCN e a Reorientação da Formação Profissional dos Cursos da Saúde”, “Sistematização da trajetória de Reorientação dos Cursos de Graduação da Saúde na UFRGS”, “Apresentação da história de Reorientação de Formação dos Cursos da Área da Saúde (videos)” e “Análise da conjuntura da saúde e cenário de resistência e perspectivas da integração Universidade e SUS”. Também houve exposições de pôsteres e tendas dos movimentos sociais. O seminário reuniu 230 participantes, contando com a presença de estudantes, professores, trabalhadores e gestores do SUS, e representantes da gestão da Universidade.
                 </span>
-                <button class="btn-leia" id="btn-seminario" onclick="ReadSeminario()">Leia mais</button>
+                <span class="font-weight-bold btn-leia" id="btn-seminario" onclick="ReadSeminario()">Leia mais</span>
             </p>
         </div>
     </div>
-    <div class="row regimento px-3 px-md-5 py-5">
+    <div class="row regimento px-3 px-md-5 py-5 my-lg-3">
         <div class="col-12 px-md-0">
             <h2 class="greenTitle text-center text-md-left mb-4">Nosso Regimento</h2>
             <p class="mb-0">
@@ -109,15 +101,17 @@
             </p>
         </div>
     </div>
-    <div class="row distrito calendario text-white">
-        <div class="col-12 col-md-8 px-md-0">
-            <h2 class="text-uppercase font-weight-bold m-0 p-md-0">Calendário de Reuniões</h2>
-            <p class="mt-4 mb-0 p-md-0 text-justify">
-                As reuniões ordinárias da Coorsaúde acontecem todas as primeiras sextas-feiras do mês, das 10 às 12 horas. Para maiores informações, <a href="mailto:coordsaude@ufrgs.br?Subject=Calendário%20de%20Reuniões" class="font-weight-bold">envie um e-mail</a> para a CoorSaúde.
-            </p>
-        </div>
-        <div class="col-4 d-none d-md-block pr-0">
-            <img src="<?php bloginfo("template_directory"); ?>/assets/calendar.svg" alt="Ícone de um calendário">
+    <div class="calendario-bg px-3">
+        <div class="row calendario text-white">
+            <div class="col-12 col-md-8 px-md-0">
+                <h2 class="text-uppercase font-weight-bold m-0 p-md-0">Calendário de Reuniões</h2>
+                <p class="mt-4 mb-0 p-md-0 text-justify">
+                    As reuniões ordinárias da Coorsaúde acontecem todas as primeiras sextas-feiras do mês, das 10 às 12 horas. Para maiores informações, <a href="mailto:coordsaude@ufrgs.br?Subject=Calendário%20de%20Reuniões" class="font-weight-bold">envie um e-mail</a> para a CoorSaúde.
+                </p>
+            </div>
+            <div class="col-4 d-none d-md-block pr-0">
+                <img src="<?php bloginfo("template_directory"); ?>/assets/calendar.svg" alt="Ícone de um calendário">
+            </div>
         </div>
     </div>
 </div>
